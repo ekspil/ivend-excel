8f3288efad5f5ed40d7b5bd658124d8b96e48f5e
devnulled <devnulled@cryptaz.org>
2019-08-18 09:54:29 +0700

    app: add log

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const Routes = require("./app/routes")
 Routes({fastify, excelService})
 
 fastify.listen(5000, "0.0.0.0", (err) => {
+    logger.info("Excel service started")
     if (err) throw err
 })
 
