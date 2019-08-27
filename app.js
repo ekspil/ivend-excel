eaefc71e944b2061d2b3f000c51fcd67941464c1
devnulled <devnulled@cryptaz.org>
2019-08-27 21:41:26 +0700

    app: logging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const ExcelService = require("./app/service/ExcelService")
 const logger = require("my-custom-logger")
+const version = require("./package").version
 
 const fastify = require("fastify")({
     logger: true
@@ -12,7 +13,7 @@ const Routes = require("./app/routes")
 Routes({fastify, excelService})
 
 fastify.listen(5000, "0.0.0.0", (err) => {
-    logger.info("Excel service started")
+    logger.info(`Excel v${version} service started`)
     if (err) throw err
 })
 
